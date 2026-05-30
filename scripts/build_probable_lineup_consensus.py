b948854d33308b05e6698327281afca38ea2b2aa
jorgemaganescobar94-lgtm <284350955+jorgemaganescobar94-lgtm@users.noreply.github.com>
2026-05-30 07:51:03 +0100

    v66.3 connect probable XI accuracy ledger

--- scripts/build_probable_lineup_consensus.py
+++ scripts/build_probable_lineup_consensus.py
@@ -7,6 +7,7 @@ from build_probable_lineup_source_registry import run as run_source_registry
 from build_autonomous_probable_lineup_collector import run as run_autonomous_collector
 from import_probable_lineup_sources import run as run_probable_source_import
 from build_probable_lineup_consensus_v2 import run as run_probable_consensus_v2
+from build_probable_lineup_accuracy_ledger import run as run_probable_accuracy_ledger
 
 
 def run(day: str, tz: str) -> None:
@@ -15,11 +16,13 @@ def run(day: str, tz: str) -> None:
     run_autonomous_collector(day, tz)
     run_probable_source_import(day, tz)
     run_probable_consensus_v2(day, tz)
+    run_probable_accuracy_ledger(day, tz)
     print("=== VSIGMA PROBABLE LINEUP CONSENSUS WRAPPER ===")
     print("source_registry=BUILT")
     print("autonomous_collector=BUILT")
     print("source_import=BUILT")
     print("consensus_engine=V2_WEIGHTED_REGISTRY")
+    print("accuracy_ledger=BUILT")
     print("auto_apply=NO")
     print("production_change=NO")
 
